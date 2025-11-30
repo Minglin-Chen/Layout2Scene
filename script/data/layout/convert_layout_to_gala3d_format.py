72fd89ad2bfc6dd43ba87ce1737156f8e02d2b81
Minglin Chen <curiosity.chenml@gmail.com>
2025-11-30 14:11:32 +0800

    update

--- script/data/layout/convert_layout_to_gala3d_format.py
+++ script/data/layout/convert_layout_to_gala3d_format.py
@@ -47,7 +47,7 @@ def transform(p, mn, mx):
     return p, scale
 
 
-def convert(layout_path, scene_type, scene_name, template_path, output_path):
+def convert(layout_path, cameras_path, scene_type, scene_name, template_path, output_dir):
     # load layout
     with open(layout_path, 'r') as f:
         layout = json.load(f)
@@ -78,33 +78,48 @@ def convert(layout_path, scene_type, scene_name, template_path, output_path):
 
         ori.append(float(b['rotation'][2]))
 
+    output_path = osp.join(output_dir, f'{scene_name}.yaml')
     write_config(template_path, output_path, scene_prompt, prompt, center, edge, ori, scene_name)
+
+    # load camera
+    with open(cameras_path, 'r') as f:
+        cameras = json.load(f)
+
+    for i in range(len(cameras)):
+        location = np.array(cameras[i]['location'])
+        location, _ = transform(location, mn, mx)
+        cameras[i]['location'] = [location[0], location[2], -location[1]]
+
+    cameras_output_path = osp.join(output_dir, f'{scene_name}_cameras.yaml')
+    with open(cameras_output_path,'w') as f:
+        json.dump(cameras, f, indent=2)
 
 
 if __name__=='__main__':
     # configuration
-    # - bedroom: 
-    #   hypersim_ai_010_005, setthescene_bedroom
-    # - livingroom: 
-    #   hypersim_ai_001_005, hypersim_ai_006_010, hypersim_ai_010_008, hypersim_ai_022_005, 
-    #   setthescene_dining_room, setthescene_living_room
-
-    scene_names = [\
-        'hypersim_ai_010_005', 'setthescene_bedroom', \
-        'hypersim_ai_001_005', 'hypersim_ai_006_010', 'hypersim_ai_010_008', 'hypersim_ai_022_005', \
-        'setthescene_dining_room', 'setthescene_living_room', \
+    scene_names = [
+        'setthescene_bedroom', 'setthescene_dining_room', 'setthescene_garage', 'setthescene_living_room',
+        'hypersim_ai_001_001', 'hypersim_ai_001_003', 'hypersim_ai_001_005', 'hypersim_ai_003_004', 'hypersim_ai_006_010', 'hypersim_ai_010_005', 'hypersim_ai_010_008', 'hypersim_ai_022_005', \
+        'bedroom_0000', 'bedroom_0001', 'bedroom_0002', 'bedroom_0003', 'bedroom_0004', 'livingroom_8013', 'livingroom_8016', 'livingroom_8017', \
+        'fankenstein_bedroom_001',
     ]
     scene_types = [
-        'bedroom', 'bedroom',
-        'livingroom', 'livingroom', 'livingroom', 'livingroom', \
-        'livingroom', 'livingroom'
+        'bedroom', 'dining room', 'garage', 'living room', \
+        'bathroom', 'office', 'dining room', 'bedroom', 'dining room', 'bedroom', 'living room', 'living room', \
+        'bedroom', 'bedroom', 'bedroom', 'bedroom', 'bedroom', 'living room', 'living room', 'living room', \
+        'bedroom',
     ]
+
+    output_dir = f'../../../relatedworks/GALA3D-main/layout_gala3d_format'
+    os.makedirs(output_dir, exist_ok=True)
 
     for scene_name, scene_type in zip(scene_names, scene_types):
         layout_path     = f'../../../data/layout/{scene_name}/layout.json'
-        output_path     = f'layout_gala3d_format/{scene_name}.yaml'
+        cameras_path    = f'../../../data/layout/{scene_name}/cameras.json'
         template_path   = 'template/gala3d_template.yaml'
 
+        assert osp.exists(layout_path) and osp.exists(cameras_path) and osp.exists(template_path)
+
         print(scene_name)
-        convert(layout_path, scene_type, scene_name, template_path, output_path)
+        convert(layout_path, cameras_path, scene_type, scene_name, template_path, output_dir)
     
